c7d1d129f1dc8836af2c84126c787727ffaf7a1a
Erik Lindmaa <guslinerz@student.gu.se>
2023-01-26 13:13:39 +0100

    bug fixes

--- WP1/bin2hec.c
+++ WP1/bin2hec.c
@@ -12,15 +12,30 @@ void getBinaryNumber(int i, int array[])
 
     k = i;
 
-    printf("\n");
-
-    printf("Right order: ");
+    // printf("Right order: ");
     for (int m = 0; m < k; m++)
     {
         passArray[m] = array[i-1];
-        printf("%d", passArray[m]);
+        // printf("%d", passArray[m]);
         i--;
     }
+
+    long hexadecimalval = 0, p = 1, remainder;
+
+    int t;
+    long long y = 0;
+    for(t = 0; t < k; t++){
+        y = 10 * y + passArray[t];
+    }
+
+    while (y != 0){
+        remainder = y % 10;
+        hexadecimalval = hexadecimalval + remainder * p;
+        p = p * 2;
+        y = y / 10;
+    }
+    printf("\nEquivalent hexadecimal value: %lX", hexadecimalval);
+
 }
     
 
--- WP1/dec2bin.c
+++ WP1/dec2bin.c
@@ -3,23 +3,40 @@
 #include <limits.h>
 #include <ctype.h>
 #include "bin2hec.c"
+#include <string.h>
 
 int storage[sizeof(long) * 8], i;
 long number;
 int k;
 
-void decimalToBin(int n)
+int decimalToBin(int n)
 {
+    int calc;
 
+    if(number <= 9){
+        calc = 4;
+    } else if (number > 9 && number <= 99) {
+        calc = 8;
+    } else if(number > 99 && number <= 999) {
+        calc = 12;
+    }  else if(number > 999 && number <= 9999){
+        calc = 16;
+    }
+
+    if(number > 4095){
+        printf("Conversion was not successful. The number has to be lower than 4096 and has to be maximum 12 bit number");
+        return 2;
+    } else{
     for (i = 0; number > 0; i++)
     {
         storage[i] = number % 2;
         number = number / 2;
     }
+    }
 
     printf("Number in binary: ");
-    
-    number = 8 - (i % 8); // Get the amount of 0 needed to format the output in an 8-bit format
+
+    number = calc - (i % calc); // Get the amount of 0 needed to format the output in an 8-bit format
     for (k = 0; k < number; k++) { // For loop that runs binMod times
         printf("%d", 0); // Prints 0
     }
@@ -36,8 +53,22 @@ void decimalToBin(int n)
 
 }
 
+    int isNumber(char number[]) {
+        int i;
+
+        for(i = 0; number[i] != 0; i++) {
+            if(!isdigit(number[i]))
+            return 1;
+        }
+       return 0; 
+    }
+
 int main(int argc, char *argv[])
 {
+    if(strcmp(argv[1], "-h") == 0){
+        printf("In order to use to program, type in a decimal number as argument.");
+        return 0;
+    } 
 
     // Store argument of switch number in shift as char array.
     char *shift = argv[1];
@@ -45,7 +76,10 @@ int main(int argc, char *argv[])
     // Converting the argument to an integer
     sscanf(shift, "%ld", &number);
 
-    if (number >= 0 && number <= LONG_MAX)
+    int test = isNumber(argv[1]);
+
+    
+    if ((number >= 0 && number <= LONG_MAX) && test == 0)
     {
         decimalToBin(number);
     }

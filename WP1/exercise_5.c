31276ec25b26ceab6a7c8020683e446e36f4587d
albinkar <guskaralbj@student.gu.se>
2023-01-26 14:00:04 +0100

    added comments

--- WP1/exercise_5.c
+++ WP1/exercise_5.c
@@ -10,37 +10,35 @@
 // This function generates a set of random numbers 
 // and fills the table *tab with these numbers 
 void create_random(int *tab ){
-    srand(time(0));
-    for(int i = 0; i < MAX; i++){
-            int random = (rand() % MAXNUMBER);
-            tab[i] = random;
-            // printf("%d,", tab[i]);
+// This is a time seed, the purpose is to get a new set of random number each time you compile the program
+    srand(time(0));                         
+    for(int i = 0; i < MAX; i++){               // Loop through the size of MAX(100)
+            int random = (rand() % MAXNUMBER);  // Assign a random number between 0 - MAXNUMBER to an int variable
+            tab[i] = random;                    // Assign the int random to the element[i] in the tab array
     }
-
-
 }
  
 // This function takes the *tab of random numbers 
 // and creates a table with the frequency counts for these numbers 
 void count_frequency(int *tab, int *freq ){
-    int counter = 0;
-    for(int i = 0; i < MAXNUMBER; i++){
-        counter = 0;
-        for(int j = 0; j < MAX; j++){
-            if(tab[j] == i){
-                counter = counter + 1;
+    int counter = 0;                            // Initializing a variable called counter with 0. To keep track of number of times a match occur
+    for(int i = 0; i < MAXNUMBER; i++){         // Loop through the freq array (0-19) 
+        counter = 0;                            // Reset the counter to 0 each new freq index
+        for(int j = 0; j < MAX; j++){           // Loop through each tab index 
+            if(tab[j] == i){                    // If there is a match between the value of tab[j] and the i then condition has met
+                counter = counter + 1;          // Add 1 to the counter 
             } 
         }
-        freq[i] = counter;
+        freq[i] = counter;                      // After the whole tab array has been looped through we assign the value of counter to the index of [i]
     }
 }     
  
 // This function takes the frequency count table 
 // and draws a histogram of the values in that frequency table 
 void draw_histogram(int *freq ){
-    int counter = 0;
-    for(int i = 0; i < MAXNUMBER; i++){
-        while(freq[i] != 0){
+    int counter = 0;                            // Initializing a variable called counter, with value 0
+    for(int i = 0; i < MAXNUMBER; i++){         // Loop through freq array using MAXNUMBER
+        while(freq[i] != 0){                    // If the value is not = to 0, then we add to the counter
             freq[i] = freq[i] -1;
             counter++;
         }
@@ -67,20 +65,19 @@ void draw_histogram(int *freq ){
 // Please modify it accordingly 
 int main (void){ 
  
-    int table[MAX], n ; 
-    int frequency[MAXNUMBER]; 
-    int *p = table;
-    create_random(p);
-    int counter1 = 0;
+    int table[MAX], n ;             // Initializing an array with MAX amount of elements 
+    int frequency[MAXNUMBER];       // Initializing an array with MAXNUMBER of elements  
+    int *p = table;                 // Initializing a pointer to where the table array address is
+    create_random(p);               // Call the create_ranom function and pass the pointer p to it
+    int counter1 = 0;                
     for (int i = 0; i < MAX; i++){
-        // printf("%d,", table[i]);
         counter1++;
     }
 
-    for (int i = 0; i < MAXNUMBER; i++){
-        frequency[i] = 0;
-    }
-    int *f = frequency;
-    count_frequency(p, f);
-    draw_histogram(f);
+    for (int i = 0; i < MAXNUMBER; i++){    // This will iterate through MAXNUMBER times over the array frequency
+        frequency[i] = 0;                   // To assign a value of 0 to each index, to later count how many
+    }                                       // times a number appears in the table array
+    int *f = frequency;                     // Initializing a pointer to where the frequency array address is
+    count_frequency(p, f);                  // Call the count_frequency function and pass p and f (pointers)
+    draw_histogram(f);                      // Call the draw_histogram function and pass f (pointer)
     } 

c3d362dfa47d75308bfe7656587e58819cceea97
Erik Lindmaa <guslinerz@student.gu.se>
2023-02-08 14:57:52 +0100

    add comments

--- WP3/exercise_2.c
+++ WP3/exercise_2.c
@@ -1,87 +1,83 @@
-float temperature;
-float celsius;
-float voltage;
-float luminosity;
-int reading;
-const unsigned long eventInterval = 1000;
-unsigned long previousTime = 0;
+float temperature;                                                   // Declare temperature variable   
+float celsius;                                                       // Declare celsius variable  
+float voltage;                                                       // Declare voltage variable  
+float luminosity;                                                    // Declare luminosity variable   
+int reading;                                                         // Declare temp meter reading variable   
+const unsigned long eventInterval = 1000;                            // Define eventInterval as one second   
+unsigned long previousTime = 0;                                      // Set previouStime to be 0   
 
-#define sensorPinZero A0
-#define sensorPinOne A1
+#define sensorPinZero A0                                             // Define A0 as sensorPinZero
+#define sensorPinOne A1                                              // Define A1 as sensorPinOne
 
 void setup()
 {
     // put your setup code here, to run once:
-    Serial.begin(9600);
+    Serial.begin(9600);                                              // Tells Arduino to get ready to exchange message with rate of 9600 bits per second. 
 }
 
 void loop()
 {
     // put your main code here, to run repeatedly:
 
-    unsigned long currentTime = millis();
-    if (currentTime - previousTime >= eventInterval)
+    unsigned long currentTime = millis();                            // Initialize unsigned long called CurrentTime with the millis function, returning number of milliseconds since start of program    
+    if (currentTime - previousTime >= eventInterval)                 // Only read light and temperature meter if currentTime - previousTime is bigger or equal to the defined event interval.    
     {
-        // read light
-        luminosity = measureLuminosity();
-        reading = analogRead(sensorPinOne);
+        luminosity = measureLuminosity();                            // Call the function for measuring luminosity, and retreiving the return value on a scale from 1-100
+        reading = analogRead(sensorPinOne);                          // Read the temperature meter pin. 
     }
 
-    // Serial.print ("Brightness:");
-    Serial.println(luminosity);
-    delay(1000);
+    Serial.println(luminosity);                                      // Print luminosity from 1-100, to make it easier when demonstrating the program   
+    delay(1000);                                                     // Delay to smoothen things out.   
 
-    voltage = reading * (5000 / 1024.0);
-    temperature = (voltage - 500) / 10;
+    voltage = reading * (5000 / 1024.0);                             // Voltage at pin in milliVolts for 5V connection  
+    temperature = (voltage - 500) / 10;                              // Converting millivolts into temeperature
 
-    // Serial.println(temperature);
-    delay(1000);
+    delay(1000);                                                     // Delay to smoothen things out. 
 
     // Correct dependencies
-    if (temperature < -12.0 && luminosity == 0.0)
+    if (temperature < -12.0 && luminosity == 0.0)                    // Check for range <-12 and luminosity 0%
     {
         digitalWrite(12, HIGH); // Turn green on
         digitalWrite(11, LOW);  // Turn red off
         digitalWrite(6, LOW);   // Turn blue off
     }
 
-    if ((temperature >= -12.0 && temperature <= 0.0) && (luminosity >= 1.0 && luminosity <= 20.0))
+    if ((temperature >= -12.0 && temperature <= 0.0) && (luminosity >= 1.0 && luminosity <= 20.0))     // Check for range 12 °C - 0 °C and luminosity 1% - 20%.
+    {
     {
         digitalWrite(12, HIGH); // Turn green on
         digitalWrite(11, LOW);  // Turn red off
         digitalWrite(6, LOW);   // Turn blue off
     }
 
-    if ((temperature <= 20.0 && temperature >= 0) && (luminosity >= 21.0 && luminosity <= 60.0))
+    if ((temperature <= 20.0 && temperature >= 0) && (luminosity >= 21.0 && luminosity <= 60.0))       // Check for range 0 °C - 20 °C and luminosity 21% - 60%.
     {
         digitalWrite(12, HIGH); // Turn green on
         digitalWrite(11, LOW);  // Turn red off
         digitalWrite(6, LOW);   // Turn blue off
     }
-
-    if ((temperature >= 21) && (luminosity >= 61.0 && luminosity <= 100.0))
-    {
+    if ((temperature >= 21) && (luminosity >= 61.0 && luminosity <= 100.0))                            // Check for range  temperature >= 21 °C and luminosity 61%-100%.
         digitalWrite(12, HIGH); // Turn green on
         digitalWrite(11, LOW);  // Turn red off
         digitalWrite(6, LOW);   // Turn blue off
     }
 
     // Incorrect dependencies temperature
-    if (temperature >= -12.0 && luminosity <= 0.0)
+    if (temperature >= -12.0 && luminosity <= 0.0)                                                    // Check if temperature > -12.0 and if luminosity is less or equal to 0.0. 
     {
         digitalWrite(11, HIGH); // Turn red on
         digitalWrite(12, LOW);  // Turn green off
         digitalWrite(6, LOW);   // Turn blue off
     }
 
-    if ((temperature > 0.0) && (luminosity >= 1.0 && luminosity <= 20.0))
+    if ((temperature > 0.0) && (luminosity >= 1.0 && luminosity <= 20.0))                             // Check if temperature > 1.0 and if luminosity is less or equal to 20.  
     {
         digitalWrite(11, HIGH); // Turn red on
         digitalWrite(12, LOW);  // Turn green off
         digitalWrite(6, LOW);   // Turn blue off
     }
 
-    if ((temperature > 20.0) && (luminosity >= 21.0 && luminosity <= 60.0))
+    if ((temperature > 20.0) && (luminosity >= 21.0 && luminosity <= 60.0))                           // Check if temperature > 20.0 and if luminosity is betwen 21% and 60%.
     {
         digitalWrite(11, HIGH); // Turn red on
         digitalWrite(12, LOW);  // Turn green off
@@ -89,21 +85,21 @@ void loop()
     }
 
     // Incorrect dependencies luminosity
-    if ((temperature < 0.0 && temperature >= -12.0) && (luminosity >= 20.0))
+    if ((temperature < 0.0 && temperature >= -12.0) && (luminosity >= 20.0))                          // Check if temperature is between -12 and 0 and if luminosity is bigger than 20%.
     {
         digitalWrite(6, HIGH); // Turn blue on
         digitalWrite(12, LOW); // Turn green off
         digitalWrite(11, LOW); // Turn red off
     }
 
-    if ((temperature < 0.0) && (luminosity >= 21.0 && luminosity <= 60.0))
+    if ((temperature < 0.0) && (luminosity >= 21.0 && luminosity <= 60.0))                             // Check if temperature is less than 0 and if luminosity is between 21% and 60%.
     {
         digitalWrite(6, HIGH); // Turn blue on
         digitalWrite(12, LOW); // Turn green off
         digitalWrite(11, LOW); // Turn red off
     }
 
-    if ((temperature < 21.0) && (luminosity >= 61.0 && luminosity <= 100.0))
+    if ((temperature < 21.0) && (luminosity >= 61.0 && luminosity <= 100.0))                          // check if temperature is less than 21 and luminosity is between 61% and 100%. 
     {
         digitalWrite(6, HIGH); // Turn blue on
         digitalWrite(12, LOW); // Turn green off
@@ -111,9 +107,9 @@ void loop()
     }
 }
 
-float measureLuminosity()
-{
-    int value_ldr = analogRead(sensorPinZero);
-    float converted_ldr_val = map(value_ldr, 6, 679, 0, 100);
-    return converted_ldr_val;
+float measureLuminosity()                                                                             // Function to check luminosity
+{       
+    int value_ldr = analogRead(sensorPinZero);                                                        // Read photometer value from pin 0  
+    float converted_ldr_val = map(value_ldr, 6, 679, 0, 100);                                         // Map value of photometer from 6-679 to 0 to 100 instead.   
+    return converted_ldr_val;                                                                         // Return converted value      
 }
--- WP3/exercise_6.c
+++ WP3/exercise_6.c
@@ -1,42 +1,44 @@
 #include <string.h>
 #include <stdio.h>
 
-void copyStringLibrary(char s[]);
-void copyString(char s[]);
-#define BUFFERSIZE 21
+void copyStringLibrary(char s[]);                   // Function declaration for function using library
+void copyString(char s[]);                          // Function declaration for manual copy
 
-int main () { 
+int main()
+{
 
-    char inputString[21];
-   
-    scanf("%s", inputString);
-    
-    copyStringLibrary(inputString);
-    copyString(inputString);
+    char inputString[25];                           // Declare char varible for the input string
+    scanf("%s", inputString);                       // Saving the inputted string
+
+    if (strlen(inputString) > 21)
+    { // If length is bigger than 21 return 0
+        printf("String too long. Try again \n");
+        return 0;
+    }
+
+    copyStringLibrary(inputString);                 // Call function using the libarary strcpy function
+    copyString(inputString);                        // Call function with the manual implementation for copying a string
 
     return 0;
-
-return 0;
 }
-
 
 void copyString(char s[])
 {
-    char secondCopy[21];
-    int i;
- 
-    for (i = 0; s[i] != '\0'; i++) {
-        secondCopy[i] = s[i];
+    char secondCopy[21];                            // Declare variable for making the second copy
+    int i;                                          // Declare int variable i to be used to loop through string
+
+    for (i = 0; s[i] != '\0'; i++)                  // Loop through string until character is \0
+    {
+        secondCopy[i] = s[i];                       // Copy over string at each position to secondCopy
     }
 
     printf("String copied with copyString method: %s\n", secondCopy);
- 
 }
 
-void copyStringLibrary(char s[]){
-    char copy[21];
-    strcpy(copy, s);
+void copyStringLibrary(char s[])
+{
+    char copy[21];                                  // Declare char variable for the copy
+    strcpy(copy, s);                                // Use strcpy to copy string to copy
 
     printf("String copied with strcpy: %s\n", copy);
-
 }
